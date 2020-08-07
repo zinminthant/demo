fb57536fa5172d4c30834d7413f1225168a3d6d2
zinmin <zinmin@blue-stone.net>
2020-08-07 10:51:34 +0630

    controller

---
+++ src/main/java/com/example/demo/controller.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller{
+	
+	
+	@RequestMapping("/")
+	public String main(){
+		return "main";
+	}
+	
+}
